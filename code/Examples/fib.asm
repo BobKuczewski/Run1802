6dda01b98fcf62d1b48bdeae7b6f4eed23cb281f
Bob <bob@ushawks.org>
2023-03-12 23:19:28 -0700

    Improved the "fib" example by removing unused code (rC was never used)

--- code/Examples/fib.asm
+++ code/Examples/fib.asm
@@ -1,32 +1,27 @@
-Ra          EQU    10
-Rb          EQU    11
-Rc          EQU    12
+START		ORG	0H
+		LDI 0
+		PHI 10
+		PHI 11
+		LDI FIB1
+		PLO 10
+		LDI FIB2
+		PLO 11
+		LDI 0
 
-START       ORG    0H
-            LDI 0
-            PHI Ra
-            PHI Rb
-            PHI Rc
-            LDI 1AH
-            PLO Ra
-            LDI 1BH
-            PLO Rb
-            LDI 0
-            PLO Rc
+LOOP		SEX 10
+		ADD
+		STR 10
+		OUT 4
+		DEC 10
+		SEX 11
+		ADD
+		STR 11
+		OUT 4
+		DEC 11
+		BR	LOOP
 
-LOOP        SEX Ra
-            ADD
-            STR Ra
-            OUT 4
-            DEC Ra
-            SEX Rb
-            ADD
-            STR Rb
-            OUT 4
-            DEC Rb
-            BR        LOOP
-            BYTE        0        ; FIB 0 2 4 6 8 ...
-            BYTE        1        ; FIB 1 3 5 7 9 ...
+FIB1  BYTE	0	; FIB 0 2 4 6 8 ...
+FIB2  BYTE	1	; FIB 1 3 5 7 9 ...
 
-            END
+		END
 
