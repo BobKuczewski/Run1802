6073884e8d801bcb7a27c6c114a455ec0e22a745
Bob <bob@ushawks.org>
2025-02-27 14:57:04 -0800

    Converted constants to EQU values in QixMono.asm (now draws green)

--- code/Examples/QixMono.asm
+++ code/Examples/QixMono.asm
@@ -1,100 +1,131 @@
 ; 1802 program to draw moving lines
 
+AdvAdd  equ	$8
+Delta   equ $9
+Coord   equ $a
+TempQ   equ $b
+OPort   equ $4
+
+MOVCMD  equ $1
+DRWCMD  equ $2
+COLCMD  equ $3
+CLRCMD  equ $4
+
+
 start   org $0
 
         ldi 0       ; Load 0
-        phi $8      ; Set upper byte of $8 to 0 for data in page 0
-        phi $9      ; Set upper byte of $9 to 0 for data in page 0
-        phi $a      ; Set upper byte of $a to 0 for data in page 0
-        phi $b      ; Set upper byte of $a to 0 for data in page 0
+        phi AdvAdd  ; Set upper byte of AdvAdd to 0 for data in page 0
+        phi Delta   ; Set upper byte of Delta to 0 for data in page 0
+        phi Coord   ; Set upper byte of Coord to 0 for data in page 0
+        phi TempQ   ; Set upper byte of TempQ to 0 for data in page 0
 
         ldi Count   ; Load the address of the Count subroutine
-        plo $8      ; Put the Count subroutine address in $8
+        plo AdvAdd  ; Put the Count subroutine address in AdvAdd
         
         ldi Out     ; Load the address of Out
-        plo $b      ; Put Out address in $b
+        plo TempQ   ; Put Out address in TempQ
         
         ; Clear the screen with black
-        sex $b    ; Use RF for output
-        ldi 4     ; Clear Screen command
-        str $b    ; Store 4 in temp
-        out 4     ; Send 4 to port 4 (automatically increments RF)
-        dec $b    ; Decrement after Out
-        ldi $00   ; Red Color = 0
-        str $b    ; Store 0 for output via X
-        out 4     ; Send 0 to port 4 (automatically increments RF)
-        dec $b    ; Return RF to point at temp (still 0)
-        out 4     ; Send 0 to port 4 (automatically increments RF)
-        dec $b    ; Return RF to point at temp (still 0)
-        out 4     ; Send 0 to port 4 (automatically increments RF)
-        dec $b    ; Return RF to point at temp (still 0)
+        sex TempQ   ; Use RF for output
+        ldi CLRCMD  ; Clear Screen command
+        str TempQ   ; Store CLRCMD in temp
+        out OPort   ; Send CLRCMD to port OPort (automatically increments X)
+        dec TempQ   ; Decrement after Out
+        ldi $00     ; Red,Grn,Blu Color = 0
+        str TempQ   ; Store 0 for output via X
+        out OPort   ; Send 0 to port OPort (automatically increments X)
+        dec TempQ   ; Return RF to point at temp (still 0)
+        out OPort   ; Send 0 to port OPort (automatically increments X)
+        dec TempQ   ; Return RF to point at temp (still 0)
+        out OPort   ; Send 0 to port OPort (automatically increments X)
+        dec TempQ   ; Return RF to point at temp (still 0)
+
+        ; Set the drawing color to green
+        sex TempQ   ; Use RF for output
+        ldi COLCMD  ; Clear Screen command
+        str TempQ   ; Store COLCMD in temp
+        out OPort   ; Send COLCMD to port OPort (automatically increments X)
+        dec TempQ   ; Decrement after Out
+        ldi $00     ; Red Color
+        str TempQ   ; Store 0 for output via X
+        out OPort   ; Send 0 to port OPort (automatically increments X)
+        dec TempQ   ; Return RF to point at temp (still 0)
+        ldi $FF     ; Green Color
+        str TempQ   ; Store 0 for output via X
+        out OPort   ; Send 0 to port OPort (automatically increments X)
+        dec TempQ   ; Return RF to point at temp (still 0)
+        ldi $00     ; Blue Color
+        str TempQ   ; Store 0 for output via X
+        out OPort   ; Send 0 to port OPort (automatically increments X)
+        dec TempQ   ; Return RF to point at temp (still 0)
         
 Loop
-        sex $b   ; Use $b for sending
+        sex TempQ   ; Use TempQ for sending
 
         ; Move to x1,y1
 
-        ldi 1    ; Move command
-        str $b   ; Store in memory
-        out 4    ; Send it
-        dec $b   ; Keep $b pointing at Out
-        ldi x1   ; Load address of x1
-        plo $a   ; $a holds address of x1
-        ldn $a   ; Load actual value of x1
-        str $b   ; Store in memory at Out
-        out 4    ; Send it
-        dec $b   ; Keep $b pointing at Out
-        ldi y1   ; Load address of y1
-        plo $a   ; $a holds address of y1
-        ldn $a   ; Load actual value of y1
-        str $b   ; Store in memory at Out
-        out 4    ; Send it
-        dec $b   ; Keep $b pointing at Out
+        ldi MOVCMD  ; Move command
+        str TempQ   ; Store in memory
+        out OPort   ; Send it
+        dec TempQ   ; Keep TempQ pointing at Out
+        ldi x1      ; Load address of x1
+        plo Coord   ; Coord holds address of x1
+        ldn Coord   ; Load actual value of x1
+        str TempQ   ; Store in memory at Out
+        out OPort   ; Send it
+        dec TempQ   ; Keep TempQ pointing at Out
+        ldi y1      ; Load address of y1
+        plo Coord   ; Coord holds address of y1
+        ldn Coord   ; Load actual value of y1
+        str TempQ   ; Store in memory at Out
+        out OPort   ; Send it
+        dec TempQ   ; Keep TempQ pointing at Out
 
         ; Draw to x2,y2
 
-        ldi 2    ; Draw command
-        str $b   ; Store in memory
-        out 4    ; Send it
-        dec $b   ; Keep $b pointing at Out
-        ldi x2   ; Load address of x2
-        plo $a   ; $a holds address of x2
-        ldn $a   ; Load actual value of x2
-        str $b   ; Store in memory at Out
-        out 4    ; Send it
-        dec $b   ; Keep $b pointing at Out
-        ldi y2   ; Load address of y2
-        plo $a   ; $a holds address of y2
-        ldn $a   ; Load actual value of y2
-        str $b   ; Store in memory at Out
-        out 4    ; Send it
-        dec $b   ; Keep $b pointing at Out
+        ldi DRWCMD  ; Draw command
+        str TempQ   ; Store in memory
+        out OPort   ; Send it
+        dec TempQ   ; Keep TempQ pointing at Out
+        ldi x2      ; Load address of x2
+        plo Coord   ; Coord holds address of x2
+        ldn Coord   ; Load actual value of x2
+        str TempQ   ; Store in memory at Out
+        out OPort   ; Send it
+        dec TempQ   ; Keep TempQ pointing at Out
+        ldi y2      ; Load address of y2
+        plo Coord   ; Coord holds address of y2
+        ldn Coord   ; Load actual value of y2
+        str TempQ   ; Store in memory at Out
+        out OPort   ; Send it
+        dec TempQ   ; Keep TempQ pointing at Out
 
         ; Move all the points
 
         ldi x1      ; Load x1
-        plo $a      ; $a holds Value
+        plo Coord   ; Coord holds Value
         ldi dx1     ; Load dx1
-        plo $9      ; $9 holds Delta
-        sep $8      ; Call the Count subroutine
+        plo Delta   ; Delta holds Delta
+        sep AdvAdd  ; Call the Count subroutine
 
         ldi y1      ; Load x1
-        plo $a      ; $a holds Value
+        plo Coord   ; Coord holds Value
         ldi dy1     ; Load dx1
-        plo $9      ; $9 holds Delta
-        sep $8      ; Call the Count subroutine
+        plo Delta   ; Delta holds Delta
+        sep AdvAdd  ; Call the Count subroutine
 
         ldi x2      ; Load x2
-        plo $a      ; $a holds Value
+        plo Coord   ; Coord holds Value
         ldi dx2     ; Load dx2
-        plo $9      ; $9 holds Delta
-        sep $8      ; Call the Count subroutine
+        plo Delta   ; Delta holds Delta
+        sep AdvAdd  ; Call the Count subroutine
 
         ldi y2      ; Load x2
-        plo $a      ; $a holds Value
+        plo Coord   ; Coord holds Value
         ldi dy2     ; Load dx2
-        plo $9      ; $9 holds Delta
-        sep $8      ; Call the Count subroutine
+        plo Delta   ; Delta holds Delta
+        sep AdvAdd  ; Call the Count subroutine
 
         br Loop     ; Branch back to continue counting
         idl
@@ -110,39 +141,39 @@ dy2     byte 8      ; dy2 will switch between + and -
 Out     byte 0      ; Used for sending a byte
 
 Return  sep $0
-Count   ; Assumes Before: [$9->delta $a->value] After [RX = $9]
+Count   ; Assumes Before: [Delta->delta Coord->value] After [RX = Delta]
         seq
         ; Check the current direction
-        ldn $9      ; Load delta via $9
+        ldn Delta   ; Load delta via Delta
         shlc        ; Shift the high bit into DF
 
         bdf CntDn   ; Branch to count down
 
 CntUp   ; Count Up Value by one Delta reversing Delta above 255
-        ldn $a      ; Value
-        sex $9      ; Set X to point at Delta (now positive)
-        add         ; D = Value + Delta (via $9 as X)
-        str $a      ; Value = Value + Delta
+        ldn Coord   ; Value
+        sex Delta   ; Set X to point at Delta (now positive)
+        add         ; D = Value + Delta (via Delta as X)
+        str Coord   ; Value = Value + Delta
         bnf UpDone  ; Done if there's no overflow
         sm          ; Subtract to undo the overflow
-        str $a      ; Update the Value
+        str Coord   ; Update the Value
         ldi 0       ; Load Zero
-        sm          ; Change sign on $9: $9 = 0-$9
-        str $9      ; Store the changed value in $9
+        sm          ; Change sign on Delta: Delta = 0-Delta
+        str Delta   ; Store the changed value in Delta
 UpDone  req
         br Return   ; Return to Main
 
 CntDn   ; Count Down Value by one Delta reversing Delta below 0
-        ldn $a      ; Value
-        sex $9      ; Set X to point at Delta (now negative)
-        add         ; D = Value + Delta (via $9 as X)
-        str $a      ; Value = Value + Delta
+        ldn Coord   ; Value
+        sex Delta   ; Set X to point at Delta (now negative)
+        add         ; D = Value + Delta (via Delta as X)
+        str Coord   ; Value = Value + Delta
         bdf DnDone  ; Done if there is an overflow
         sm          ; Subtract to undo the overflow
-        str $a      ; Update the Value
+        str Coord   ; Update the Value
         ldi 0       ; Load Zero
-        sm          ; Change sign on $9: $9 = 0-$9
-        str $9      ; Store the changed value in $9
+        sm          ; Change sign on Delta: Delta = 0-Delta
+        str Delta   ; Store the changed value in Delta
 DnDone  req
         br Return   ; Return to Main
 
