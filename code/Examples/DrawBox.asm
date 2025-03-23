bf15eefac8a4d5c1866b97ce8925d6a25d5b05e9
Bob <bob@ushawks.org>
2025-03-23 12:53:49 -0700

    Updated the DrawBox.asm program with comments

--- code/Examples/DrawBox.asm
+++ code/Examples/DrawBox.asm
@@ -1,42 +1,46 @@
-; 1802 program to draw a number "4"
+; 1802 program to draw a box
 
-START  ORG 00H
-       LDI 00
-       PHI 0FH
-       PHI 0EH
-       LDI DATA
-       PLO 0FH
-       SEX 0FH
-       LDI EDATA-DATA  ; 13H
-       PLO 0EH
-SEND   OUT 4
-       DEC 0EH
-       GLO 0Eh
-       BNZ SEND
-       IDL
+GPort  EQU 6
 
-DATA   BYTE 03
-       BYTE 0FFH 
-       BYTE 00
-       BYTE 00
-       BYTE 01
-       BYTE 01
-       BYTE 01
-       BYTE 02
-       BYTE 0FFH
-       BYTE 01
-       BYTE 02
-       BYTE 0FFH
-       BYTE 0FFH
-       BYTE 02
-       BYTE 01
-       BYTE 0FFH
-       BYTE 02
-       BYTE 01
-       BYTE 01
-EDATA
+START  ORG $00
+       LDI 00     ; Load 0 for upper address registers
+       PHI $0F    ; Zero high byte of F
+       PHI $0E    ; Zero high byte of E
+       LDI DATA   ; Load the address of the graphics commands
+       PLO $0F    ; Point F at the graphics commands
+       SEX $0F    ; Use F as X to send the graphics commands
+       LDI EDATA-DATA  ; Load the size of the graphics commands
+       PLO $0E    ; Use E to count down while sending commands
+SEND   OUT GPort  ; Output the next byte of graphics commands
+       DEC $0E    ; Decrement the count down register
+       GLO $0E    ; Get the current count
+       BNZ SEND   ; Branch to send more as needed
+       IDL        ; Done!!
 
+; Graphics commands to send
+DATA   BYTE $03   ; Set Color
+       BYTE $00   ; Red: 00
+       BYTE $FF   ; Green: FF
+       BYTE $00   ; Blue: 00
+       BYTE $01   ; Move to ...
+       BYTE $50   ; x: 50x
+       BYTE $50   ; y: 50x
+       BYTE $02   ; Draw to ...
+       BYTE $B0   ; x: B0x
+       BYTE $50   ; y: 50x
+       BYTE $02   ; Draw to ...
+       BYTE $B0   ; x: B0x
+       BYTE $B0   ; y: B0x
+       BYTE $02   ; Draw to ...
+       BYTE $50   ; x: 50x
+       BYTE $B0   ; y: B0x
+       BYTE $02   ; Draw to ...
+       BYTE $50   ; x: 50x
+       BYTE $50   ; y: B0x
+       BYTE $03   ; Set Color (to send extra bytes)
+       BYTE $00   ; Red: 00
+       BYTE $00   ; Green: 00
+       BYTE $00   ; Blue: 00
+EDATA             ; Label to measure number of graphics commands
 		   END
-
-
 
